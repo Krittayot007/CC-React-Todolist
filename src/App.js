007b7c5b574bcd5cd1510a2021059173ec2a28d1
Krittayot007 <krittayot.som@gmail.com>
2023-04-24 16:14:08 +0700

    finish:Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
+import { FaHome } from 'react-icons/fa';
 
 function App() {
   return (
     <div className='container'>
       {/* Header */}
-      <header className='header'>Header</header>
-
+      <header className='header'> 
+        <span>
+          <FaHome />
+        </span>
+        <div>CC-Todolist</div> 
+      </header>
       {/* Sidebar */}
       <aside className='sidebar'>Sidebar</aside>
 
