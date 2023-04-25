e711414f4444677faeb4d99bac4ac94ad249f2da
Krittayot007 <krittayot.som@gmail.com>
2023-04-25 10:13:52 +0700

    finish:UI todo-content

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import { Sidebar } from "./components/Sidebar";
 import { TodoContent } from "./components/Todo/TodoContent";
 
 function App() {
-
-  const mockTodo = Array.from({length:100},(el,idx) => idx+1)
-  // console.log(mockTodo);
   return (
     <div className="container">
       {/* Header */}
